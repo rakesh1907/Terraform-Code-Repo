bfec09ebd98e6817bce50ae3c6c6415c679a2dd5
Rakesh Patil <rakesh.patil@gmail.com>
2025-11-11 13:09:50 +0530

    terraform_SG groupd added

--- Terraform-EC2.tf
+++ Terraform-EC2.tf
@@ -5,8 +5,42 @@ provider "aws" {
 resource "aws_instance" "Terraform-Server"{
     ami = "ami-02d26659fd82cf299"
     instance_type = "t3.micro"
+    
+    # Lifecycle block ensures instance state is managed
+    # When instance is stopped, terraform apply will automatically start it
+    lifecycle {
+        ignore_changes = []
+    }
+    
     tags = {
       Name = "Terraform-Server"
     }
 }
+
+resource "aws_security_group" "SG_Terraform" {
+  name        = "SG_Terraform"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-0fbf112c2ed57676a"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv4         = aws_vpc.main.cidr_block
+#   from_port         = 443
+#   ip_protocol       = "tcp"
+#   to_port           = 443
+# }
+
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1" # semantically equivalent to all ports
+# }
+
+
 
